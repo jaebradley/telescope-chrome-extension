f39e773b91f23ecb792a870f2907a1ac1f544dda
Jae Bradley <jae.b.bradley@gmail.com>
2019-09-22 18:11:59 -0700

    Refactor logic (#5)

--- src/content/App.jsx
+++ src/content/App.jsx
@@ -3,12 +3,9 @@ import React, {
   useState,
 } from 'react';
 import axios from 'axios';
-// import Popover from '@material-ui/core/Popover';
 import Portal from '@material-ui/core/Portal';
 import CloseIcon from '@material-ui/icons/Close';
 import IconButton from '@material-ui/core/IconButton';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import {
   makeStyles,
 } from '@material-ui/styles';
@@ -17,6 +14,13 @@ import classnames from 'classnames';
 import LeaderDetails from './LeaderDetails';
 import FeaturedReview from './FeaturedReview';
 import Ratings from './Ratings';
+import {
+  API_BASE_URL,
+  APP_ELEMENT_ID,
+  DEFAULT_DATA,
+} from './constants';
+import transformEmployer from './data/transformEmployer';
+import UnableToIdentifyCompany from './UnableToIdentifyCompany';
 
 const useStyles = makeStyles({
   root: {
@@ -30,52 +34,13 @@ const useStyles = makeStyles({
   },
 });
 
-const DEFAULT_DATA = {
-  name: '',
-  industryName: '',
-  logoURL: null,
-  websiteURL: null,
-  reviewsURL: null,
-  ratings: {
-    careerOpportunities: null,
-    compensationAndBenefits: null,
-    workLifeBalance: null,
-    overall: null,
-    seniorLeadership: null,
-    count: null,
-    description: '',
-  },
-  leader: {
-    name: '',
-    ratingsCount: null,
-    approvalPercentage: null,
-    disapprovalPercentage: null,
-    title: '',
-    image: {
-      url: null,
-      height: null,
-      width: null,
-    },
-  },
-  featuredReview: {
-    url: null,
-    consDescription: '',
-    prosDescription: '',
-    jobTitle: '',
-    headline: '',
-    reviewDateTime: null,
-    location: null,
-    overallRating: null,
-  },
-};
-
 const isValidResponse = (response) => !!response
   && !!response.data
   && !!response.data.response
   && !!response.data.response.employers
   && !!response.data.response.employers.length;
 
-export default function SimplePopover() {
+export default function App() {
   const classes = useStyles();
   const [
     data,
@@ -94,65 +59,29 @@ export default function SimplePopover() {
     setSelectedText,
   ] = useState('');
 
-  const anchorEl = document.getElementById('telescope');
+  const anchorEl = document.getElementById(APP_ELEMENT_ID);
 
   useEffect(() => {
     chrome.extension.onMessage.addListener(({ selectionText }) => {
       setSelectedText(selectionText);
 
-      axios.get('https://telescope-chrome-extension.herokuapp.com/employers', {
-        params: {
-          search_term: selectionText,
-        },
-      }).then((response) => {
-        if (isValidResponse(response)) {
-          const responseData = response.data.response;
-          const firstEmployer = responseData.employers[0];
-          setData({
-            name: firstEmployer.name,
-            industryName: firstEmployer.industryName,
-            logoURL: firstEmployer.squareLogo,
-            websiteURL: firstEmployer.website,
-            reviewsURL: responseData.attributionURL,
-            ratings: {
-              careerOpportunities: firstEmployer.careerOpportunitiesRating,
-              compensationAndBenefits: firstEmployer.compensationAndBenefitsRating,
-              workLifeBalance: firstEmployer.workLifeBalanceRating,
-              overall: firstEmployer.overallRating,
-              seniorLeadership: firstEmployer.seniorLeadershipRating,
-              count: firstEmployer.numberOfRatings,
-              description: firstEmployer.ratingDescription,
-            },
-            leader: firstEmployer.ceo && {
-              title: firstEmployer.ceo.title,
-              name: firstEmployer.ceo.name,
-              ratingsCount: firstEmployer.ceo.numberOfRatings,
-              approvalPercentage: firstEmployer.ceo.pctApprove,
-              disapprovalPercentage: firstEmployer.ceo.pctDisapprove,
-              image: {
-                url: firstEmployer.ceo.image.src,
-                height: firstEmployer.ceo.image.height,
-                width: firstEmployer.ceo.image.width,
-              },
-            },
-            featuredReview: {
-              url: firstEmployer.featuredReview.attributionURL,
-              consDescription: firstEmployer.featuredReview.cons,
-              prosDescription: firstEmployer.featuredReview.pros,
-              jobTitle: firstEmployer.featuredReview.jobTitle,
-              headline: firstEmployer.featuredReview.headline,
-              reviewDateTime: firstEmployer.featuredReview.reviewDateTime,
-              location: firstEmployer.featuredReview.location,
-              overallRating: firstEmployer.featuredReview.overall,
-            },
-          });
-          setAbleToIdentifyCompany(true);
-        } else {
-          setData(DEFAULT_DATA);
-          setAbleToIdentifyCompany(false);
-        }
-        setOpen(true);
-      });
+      axios
+        .get(API_BASE_URL, { params: { search_term: selectionText } })
+        .then((response) => {
+          if (isValidResponse(response)) {
+            const responseData = response.data.response;
+            const firstEmployer = responseData.employers[0];
+            setData({
+              ...transformEmployer(firstEmployer),
+              reviewsURL: responseData.attributionURL,
+            });
+            setAbleToIdentifyCompany(true);
+          } else {
+            setData(DEFAULT_DATA);
+            setAbleToIdentifyCompany(false);
+          }
+          setOpen(true);
+        });
     });
   });
 
@@ -173,17 +102,7 @@ export default function SimplePopover() {
         <IconButton onClick={() => setOpen(false)}>
           <CloseIcon />
         </IconButton>
-        {
-          !ableToIdentifyCompany && (
-            <Paper className={classes.unableToIdentifyCompany}>
-              <Typography variant="h4">
-                Unable to identify company for selected text:
-                {' '}
-                {`'${selectedText}'`}
-              </Typography>
-            </Paper>
-          )
-        }
+        { !ableToIdentifyCompany && <UnableToIdentifyCompany selectedText={selectedText} /> }
         {
           ableToIdentifyCompany && (
             <>
