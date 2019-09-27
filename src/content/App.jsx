2e76de233869eae9742f24fdc5d8422db9bb3abc
Jae Bradley <jae.b.bradley@gmail.com>
2019-09-26 22:40:19 -0700

    Use custom react hook to convert company name to an input

--- src/content/App.jsx
+++ src/content/App.jsx
@@ -3,7 +3,6 @@ import React, {
   useState,
   useCallback,
 } from 'react';
-import axios from 'axios';
 import Portal from '@material-ui/core/Portal';
 import CloseIcon from '@material-ui/icons/Close';
 import IconButton from '@material-ui/core/IconButton';
@@ -25,11 +24,10 @@ import LeaderDetails from './LeaderDetails';
 import FeaturedReview from './FeaturedReview';
 import Ratings from './Ratings';
 import {
-  API_BASE_URL,
   APP_ELEMENT_ID,
 } from './constants';
-import transformEmployer from './data/transformEmployer';
 import UnableToIdentifyCompany from './UnableToIdentifyCompany';
+import useCompanySearch from './hooks/useCompanySearch';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -70,38 +68,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const isValidResponse = (response) => !!response
-  && !!response.data
-  && !!response.data.response
-  && !!response.data.response.employers
-  && !!response.data.response.employers.length;
-
 export default function App() {
   const classes = useStyles();
-  const [
-    isLoading,
-    setIsLoading,
-  ] = useState(true);
-  const [
-    currentCompanyIndex,
-    setCurrentCompanyIndex,
-  ] = useState(null);
-  const [
-    companies,
-    setCompanies,
-  ] = useState([]);
   const [
     open,
     setOpen,
   ] = useState(false);
-  const [
-    ableToIdentifyCompany,
-    setAbleToIdentifyCompany,
-  ] = useState(false);
-  const [
-    selectedText,
-    setSelectedText,
-  ] = useState('');
   const [
     inputText,
     setInputText,
@@ -109,90 +81,49 @@ export default function App() {
 
   const anchorEl = document.getElementById(APP_ELEMENT_ID);
 
-  useEffect(() => {
-    const handleSelectedText = (text) => {
-      setIsLoading(true);
-      setSelectedText(text);
+  const {
+    search,
+    companies,
+    currentCompanyIndex,
+    searching,
+    searchTerm,
+    setCurrentCompanyIndex,
+  } = useCompanySearch();
 
-      axios
-        .get(API_BASE_URL, { params: { search_term: text } })
-        .then((response) => {
-          if (isValidResponse(response)) {
-            const responseData = response.data.response;
-            setCompanies(responseData.employers.slice(0, 5).map(transformEmployer));
-            setCurrentCompanyIndex(0);
-            setInputText(responseData.employers[0].name);
-            setAbleToIdentifyCompany(true);
-          } else {
-            setCompanies([]);
-            setCurrentCompanyIndex(null);
-            setInputText('');
-            setAbleToIdentifyCompany(false);
-          }
-          setIsLoading(false);
-          setOpen(true);
-        }).catch(() => {
-          setCompanies([]);
-          setCurrentCompanyIndex(null);
-          setAbleToIdentifyCompany(false);
-          setInputText('');
-          setIsLoading(false);
-          setOpen(true);
-        });
+  useEffect(() => {
+    const handleSelectedSearchTerm = ({ selectionText }) => {
+      if (!open) {
+        setOpen(true);
+      }
+
+      setInputText(selectionText);
+      search(selectionText);
     };
 
-    chrome.extension.onMessage.addListener(({ selectionText }) => handleSelectedText(selectionText));
-    return chrome.extension.onMessage.removeListener(handleSelectedText);
-  }, []);
-
-  const handleInputChange = useCallback((value) => {
-    setIsLoading(true);
-    setSelectedText(value);
-    axios
-      .get(API_BASE_URL, { params: { search_term: value } })
-      .then((response) => {
-        if (isValidResponse(response)) {
-          const responseData = response.data.response;
-          setCompanies(responseData.employers.slice(0, 5).map(transformEmployer));
-          setCurrentCompanyIndex(0);
-          setInputText(responseData.employers[0].name);
-          setAbleToIdentifyCompany(true);
-        } else {
-          setCompanies([]);
-          setCurrentCompanyIndex(null);
-          setInputText('');
-          setAbleToIdentifyCompany(false);
-        }
-        setIsLoading(false);
-        setOpen(true);
-      }).catch(() => {
-        setCompanies([]);
-        setCurrentCompanyIndex(null);
-        setAbleToIdentifyCompany(false);
-        setInputText('');
-        setIsLoading(false);
-        setOpen(true);
-      });
-  }, []);
+    chrome.extension.onMessage.addListener(handleSelectedSearchTerm);
+    return () => chrome.extension.onMessage.removeListener(handleSelectedSearchTerm);
+  });
 
   const [
     debouncedInputChangeHandler,
-  ] = useDebouncedCallback(handleInputChange, 500);
+  ] = useDebouncedCallback(search, 500);
 
   const handleViewingPreviousCompany = useCallback(() => {
-    if (currentCompanyIndex <= 0) {
-      setCurrentCompanyIndex(companies.length - 1);
-    } else {
-      setCurrentCompanyIndex(currentCompanyIndex - 1);
-    }
+    const nextCompanyIndex = currentCompanyIndex <= 0
+      ? companies.length - 1
+      : currentCompanyIndex - 1;
+
+    setCurrentCompanyIndex(nextCompanyIndex);
+    setInputText(companies[nextCompanyIndex].name);
   }, [currentCompanyIndex, companies]);
 
   const handleViewingNextCompany = useCallback(() => {
-    if (currentCompanyIndex >= companies.length - 1) {
-      setCurrentCompanyIndex(0);
-    } else {
-      setCurrentCompanyIndex(currentCompanyIndex + 1);
-    }
+    const nextCompanyIndex = currentCompanyIndex >= companies.length - 1
+      ? 0
+      : currentCompanyIndex + 1;
+
+    setCurrentCompanyIndex(nextCompanyIndex);
+    setInputText(companies[nextCompanyIndex].name);
   }, [currentCompanyIndex, companies]);
 
   return (
@@ -232,51 +163,56 @@ export default function App() {
           </Toolbar>
         </AppBar>
         {
-          isLoading && (
+          searching && (
             <Paper className={classes.header} classes={{ root: classes.loader }}>
               <CircularProgress />
             </Paper>
           )
         }
-        { !isLoading && !ableToIdentifyCompany && <UnableToIdentifyCompany selectedText={selectedText} /> }
+        { !searching && !companies.length && <UnableToIdentifyCompany selectedText={searchTerm} /> }
         {
-          !isLoading && ableToIdentifyCompany && (
-            <>
-              <Paper className={classes.header}>
-                <IconButton
-                  className={classes.primary}
-                  onClick={handleViewingPreviousCompany}
-                  disabled={!companies || companies.length <= 1}
-                >
-                  <KeyboardArrowLeftIcon />
-                </IconButton>
-                <Input
-                  classes={{ input: classes.input }}
-                  value={inputText}
-                  disableUnderline
-                  onChange={(e) => { setInputText(e.target.value); debouncedInputChangeHandler(e.target.value); }}
+          !searching
+            && companies.length
+            && (
+              <>
+                <Paper className={classes.header}>
+                  <IconButton
+                    className={classes.primary}
+                    onClick={handleViewingPreviousCompany}
+                    disabled={!companies || companies.length <= 1}
+                  >
+                    <KeyboardArrowLeftIcon />
+                  </IconButton>
+                  <Input
+                    classes={{ input: classes.input }}
+                    value={inputText}
+                    disableUnderline
+                    onChange={(e) => {
+                      setInputText(e.target.value);
+                      debouncedInputChangeHandler(e.target.value);
+                    }}
+                  />
+                  <IconButton
+                    className={classes.primary}
+                    onClick={handleViewingNextCompany}
+                    disabled={!companies || companies.length <= 1}
+                  >
+                    <KeyboardArrowRightIcon />
+                  </IconButton>
+                </Paper>
+                <Ratings
+                  logoURL={companies[currentCompanyIndex].logoURL}
+                  companyName={companies[currentCompanyIndex].name}
+                  data={companies[currentCompanyIndex].ratings}
                 />
-                <IconButton
-                  className={classes.primary}
-                  onClick={handleViewingNextCompany}
-                  disabled={!companies || companies.length <= 1}
-                >
-                  <KeyboardArrowRightIcon />
-                </IconButton>
-              </Paper>
-              <Ratings
-                logoURL={companies[currentCompanyIndex].logoURL}
-                companyName={companies[currentCompanyIndex].name}
-                data={companies[currentCompanyIndex].ratings}
-              />
-              {
-                companies[currentCompanyIndex].leader && <LeaderDetails data={companies[currentCompanyIndex].leader} />
-              }
-              {
-                companies[currentCompanyIndex].featuredReview && <FeaturedReview data={companies[currentCompanyIndex].featuredReview} />
-              }
-            </>
-          )
+                {
+                  companies[currentCompanyIndex].leader && <LeaderDetails data={companies[currentCompanyIndex].leader} />
+                }
+                {
+                  companies[currentCompanyIndex].featuredReview && <FeaturedReview data={companies[currentCompanyIndex].featuredReview} />
+                }
+              </>
+            )
         }
       </div>
     </Portal>
