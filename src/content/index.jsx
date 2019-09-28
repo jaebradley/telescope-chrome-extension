554423588cb75f6d86f502c7ec600bdcc36ec513
Jae Bradley <jae.b.bradley@gmail.com>
2019-09-28 01:29:53 -0700

    Fixes before v1.0.0 publish

--- src/content/index.jsx
+++ src/content/index.jsx
@@ -13,7 +13,8 @@ import theme from './theme';
 const app = document.createElement('div');
 app.id = APP_ELEMENT_ID;
 if (window.location.host === 'www.linkedin.com') {
-  app.setAttribute('style', 'z-index: 10000; position: fixed; left: 80%; top: 75px; width: 20%;');
+  // The widget should live on the right-hand side of the screen, with a width of 250px, and be 20px from the right-edge of screen
+  app.setAttribute('style', 'z-index: 10000; position: fixed; left: calc(100% - 270px); top: 75px; padding-right: 20px;');
 
   if (document.getElementsByClassName('nav-main__content').length) {
     document.getElementsByClassName('nav-main__content')[0].appendChild(app);
