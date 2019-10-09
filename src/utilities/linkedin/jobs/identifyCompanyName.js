bdc33ebc9cd225b06f052e02f70b5f4e165ede77
Jae Bradley <jae.b.bradley@gmail.com>
2019-10-08 23:10:41 -0700

    Support updating company search on a company page

--- src/utilities/linkedin/jobs/identifyCompanyName.js
+++ src/utilities/linkedin/jobs/identifyCompanyName.js
@@ -1,5 +1,6 @@
 import isValidJobPage from './isValidJobPage';
 import isValidJobSearchPage from './isValidJobSearchPage';
+import isValidCompanyPage from './isValidCompanyPage';
 
 export default function identifyCompanyName(url) {
   if (isValidJobPage(url)) {
@@ -10,6 +11,13 @@ export default function identifyCompanyName(url) {
   if (isValidJobSearchPage(url)) {
     const companyLink = document.querySelector('a.jobs-details-top-card__company-url');
     return companyLink.text.trim();
+  }
+
+  if (isValidCompanyPage(url)) {
+    const companyTitleHeader = document.querySelector('h1.org-top-card-summary__title');
+    if (companyTitleHeader) {
+      return companyTitleHeader.textContent.trim();
+    }
   }
 
   return null;
