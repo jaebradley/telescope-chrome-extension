72d5ffe79020aa605b93cca79e9384fafda4bc0e
Jae Bradley <jae.b.bradley@gmail.com>
2019-10-08 23:52:49 -0700

    LinkedIn support profiles

--- src/utilities/linkedin/jobs/identifyCompanyName.js
+++ src/utilities/linkedin/jobs/identifyCompanyName.js
@@ -1,6 +1,7 @@
 import isValidJobPage from './isValidJobPage';
 import isValidJobSearchPage from './isValidJobSearchPage';
 import isValidCompanyPage from './isValidCompanyPage';
+import isValidProfilePage from './isValidProfilePage';
 
 export default function identifyCompanyName(url) {
   if (isValidJobPage(url)) {
@@ -17,6 +18,34 @@ export default function identifyCompanyName(url) {
     const companyTitleHeader = document.querySelector('h1.org-top-card-summary__title');
     if (companyTitleHeader) {
       return companyTitleHeader.textContent.trim();
+    }
+  }
+
+  if (isValidProfilePage(url)) {
+    let companyNameContainer = document.querySelector('li[data-control-name="position_see_more"]');
+    if (companyNameContainer) {
+      return companyNameContainer.textContent.trim();
+    }
+
+    companyNameContainer = document.querySelector(
+      'a[data-control-name="background_details_company"] > div.pv-entity__summary-info > p.pv-entity__secondary-title',
+    );
+    if (companyNameContainer) {
+      return companyNameContainer.textContent.trim();
+    }
+
+    companyNameContainer = document.querySelector(
+      'a[data-control-name="background_details_company"] > div.pv-entity__summary-info > p > span:not(.visually-hidden)',
+    );
+    if (companyNameContainer) {
+      return companyNameContainer.textContent.trim();
+    }
+
+    companyNameContainer = document.querySelector(
+      'a[data-control-name="background_details_company"] > div.pv-entity__company-summary-info > h3 > span:not(.visually-hidden)',
+    );
+    if (companyNameContainer) {
+      return companyNameContainer.textContent.trim();
     }
   }
 
--- src/utilities/linkedin/jobs/shouldIdentifyCompanyName.js
+++ src/utilities/linkedin/jobs/shouldIdentifyCompanyName.js
@@ -1,7 +1,8 @@
 import isValidJobPage from './isValidJobPage';
 import isValidJobSearchPage from './isValidJobSearchPage';
 import isValidCompanyPage from './isValidCompanyPage';
+import isValidProfilePage from './isValidProfilePage';
 
 export default function shouldIdentifyCompanyName(url) {
-  return isValidJobPage(url) || isValidJobSearchPage(url) || isValidCompanyPage(url);
+  return isValidJobPage(url) || isValidJobSearchPage(url) || isValidCompanyPage(url) || isValidProfilePage(url);
 }
